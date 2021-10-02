f246d8821e9782cfe4286aa066acec80af06cead
jonkwee <jonkwee@gmail.com>
2021-10-01 22:27:12 -0500

    Added square's onclick action to change color

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -1,14 +1,33 @@
-function Square({stats}) {
-    return (
-        <button className="square">
-            <div className="content-container">
-                Top: {stats.top}
-                Left: {stats.left}
-                Right: {stats.right}
-                Bottom: {stats.bottom}
-            </div>
-        </button>
-    )
+import React, { Component } from 'react'
+
+class Square extends Component {
+    constructor() {
+        super()
+        this.state = {
+            selected: false
+        }
+    }
+
+    squareClicked() {
+        this.setState(prevState => ({
+            selected: !prevState.selected 
+        }), console.log("Clicked"))
+    }
+
+    render() {
+        return (
+            <button onClick={() => this.squareClicked()} className={this.state.selected ? "square border-selected" : "square border-normal"}>
+                <div className="content-container">
+                    <div className="content-top">{this.props.stats.top}</div>
+                    <div className="content-middle">
+                        <div className="content-left">{this.props.stats.left}</div>
+                        <div className="content-right">{this.props.stats.right}</div>
+                    </div>
+                    <div className="content-bottom">{this.props.stats.bottom}</div>
+                </div>
+            </button>
+        )
+    }
 }
 
 export default Square;
